1e552b93931f21f0b2b0c0ce75a68ef883cdb42f
vanderfsouza <45627970+vanderfsouza@users.noreply.github.com>
2019-01-10 09:10:16 -0200

    Update Aula_2.R

--- Curso_R_Aula_2/Aula_2.R
+++ Curso_R_Aula_2/Aula_2.R
@@ -285,4 +285,24 @@ points(d,med_t)
 x = seq(0,300,0.1)
 lines(x, (predict(lm(peso ~ I(d)+I(d^2)),data.frame(d=x))),lty=2, col="blue")
 
-#
+#######
+# Extra
+myPlot<-function(beta){
+  y<-galton$child-mean(galton$child)
+  x<-galton$parent-mean(galton$parent)
+  freqData<-as.data.frame(table(x,y))
+  names(freqData)<-c("child","parent","freq")
+  plot(
+    as.numeric(as.vector(freqData$parent)),
+    as.numeric(as.vector(freqData$child)),
+    pch=21,col="black",bg="lightblue",
+    cex=.15*freqData$freq,
+    xlab="parent",
+    ylab="child"
+  )
+  abline(0,beta,lwd=3)
+  points(0,0,cex=2,pch=19)
+  mse<-mean((y-beta*x)^2)
+  title(paste("beta=",beta,"mse=",round(mse,3)))
+}
+manipulate(myPlot(beta),beta=slider(0,1,step=0.02))
